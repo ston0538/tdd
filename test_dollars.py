9c4ef1d3c0eff108971b02f24ed2a1e0ffbd5f3c
KHK <ston0538@gmail.com>
2020-03-26 01:32:11 +0900

    2장Dollar의 부작용
    - [ ]  $5 + 10CHF = $10(환율이 2:1인 경우)
    - [o]  $5 x 2 = $10
    - [ ]  amount를 private으로 만들기
    - [o]  Dollar 부작용?
    - [ ]  Money 반올림?
    
    https://www.notion.so/2-c13f591a5a9d4d15a459344616109b5b

--- test_dollars.py
+++ test_dollars.py
@@ -3,7 +3,7 @@ from money.dollar import Dollar
 
 def test_multiplication():
     five = Dollar(5)
-    five.times(2)
-    assert five.amount == 10
-
-# NameError: name 'Dollar' is not defined
+    product = five.times(2)
+    assert product.amount == 10
+    product = five.times(3)
+    assert product.amount == 15
